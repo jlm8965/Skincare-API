16d5757d972720d524e1a4a1082d297715a93494
jlm8965 <JulieLMonsees@gmail.com>
2022-10-04 02:11:11 -0500

    last commit

---
+++ models/skincare.js
@@ -0,0 +1,43 @@
+//////////////////////////////////////////////
+/// Our schme and model for the fruit resource
+///////////////////////////////////////////////
+
+//import mongoose again
+const mongoose = require("mongoose") 
+
+//we're going to pull the Schema and model from mongoose
+//we'll use a syntax called "destructuring"
+//pulling Schema (which is a class) and a model (which is a function) out of mongoose
+//you can use a class like a function
+
+const {Schema, model} = mongoose
+
+//view = the information you're getting back from a request to the server
+//sometimes it's going to be JSON, sometimes HTML in a browser. 
+
+
+//skincare products schema 
+//making a new instance of the class Schema
+//passing in an object as an argument
+//our database is going to hold a collection of skincare products documents
+//SCHEMA SETS THE RULES 
+const skincareSchema = new Schema({
+    name: String, 
+    formulation: String, 
+    whitecast: Boolean
+
+}) 
+
+//make the skincare model
+//MODEL MAKES IT HAPPEN 
+//turning schema into a model
+//calling the model method
+//the model method takes two arguments: the first is what we wil call our model
+//the second is what we will use to build the model 
+const Fruit = model("Skincare", skincareSchema)
+
+
+//////////////////////////////////////////////
+/// Export the model
+///////////////////////////////////////////////
+module.exports = Skincare
